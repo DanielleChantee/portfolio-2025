5c4c9c049a4af9131d63550f53504bb7ec6a5bb2
Danielle <daniellechantee@gmail.com>
2024-11-13 01:10:06 +0000

    updates

--- scripts.js
+++ scripts.js
@@ -1,42 +1,45 @@
 document.addEventListener("DOMContentLoaded", function () {
-  var splide1 = new Splide("#carousel", {
-    perPage: 4,
-    rewind: true,
-    autoplay: true,
-    interval: 3000,
-    pauseOnHover: false,
-    pauseOnFocus: false,
-    breakpoints: {
-      1410: {
-        perPage: 3,
+  var carouselElement = document.querySelector("#carousel");
+  if (carouselElement) {
+    var carousel = new Splide(carouselElement, {
+      perPage: 4,
+      rewind: true,
+      autoplay: true,
+      interval: 3000,
+      pauseOnHover: false,
+      pauseOnFocus: false,
+      breakpoints: {
+        1410: {
+          perPage: 3,
+        },
+        1100: {
+          perPage: 2,
+        },
+        800: {
+          perPage: 1,
+        },
       },
-      1100: {
-        perPage: 2,
-      },
-      800: {
-        perPage: 1,
-      },
-    },
-  });
+    });
 
-  splide1.mount();
+    carousel.mount();
+  }
 
-  var splide2 = new Splide("#featured-clients", {
-    perPage: 2,
-    rewind: true,
-    autoplay: true,
-    interval: 3000,
-    pauseOnHover: false,
-    pauseOnFocus: false,
-    breakpoints: {
-      1100: {
-        perPage: 2,
+  var featuredClientsElement = document.querySelector("#featured-clients");
+  if (featuredClientsElement) {
+    var featuredClients = new Splide(featuredClientsElement, {
+      perPage: 2,
+      rewind: true,
+      autoplay: true,
+      interval: 4000,
+      pauseOnHover: false,
+      pauseOnFocus: false,
+      breakpoints: {
+        768: {
+          perPage: 1,
+        },
       },
-      800: {
-        perPage: 1,
-      },
-    },
-  });
+    });
 
-  splide2.mount();
+    featuredClients.mount();
+  }
 });
